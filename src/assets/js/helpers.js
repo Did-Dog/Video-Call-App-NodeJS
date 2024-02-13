e688d3662c5d2f45b5a98e29aac886677057c71b
DidDog <94751052+Did-Dog@users.noreply.github.com>
2024-02-13 20:41:56 +0530

    Update helpers.js

--- src/assets/js/helpers.js
+++ src/assets/js/helpers.js
@@ -111,14 +111,13 @@ export default {
         return {
             iceServers: [
                 {
-                    urls: ["stun:eu-turn4.xirsys.com"]
+                    urls: ["stun:turn01.brie.fi:5349"]
                 },
                 {
-                    username: "ml0jh0qMKZKd9P_9C0UIBY2G0nSQMCFBUXGlk6IXDJf8G2uiCymg9WwbEJTMwVeiAAAAAF2__hNSaW5vbGVl",
-                    credential: "4dd454a6-feee-11e9-b185-6adcafebbb45",
+                    username: "brie",
+                    credential: "fi",
                     urls: [
-                        "turn:eu-turn4.xirsys.com:80?transport=udp",
-                        "turn:eu-turn4.xirsys.com:3478?transport=tcp"
+                        "turn:turn01.brie.fi:5349"
                     ]
                 }
             ]
